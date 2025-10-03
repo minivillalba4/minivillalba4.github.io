c474d50c512abcdbd8114d3bc3ecb7ac32747b2c
minivillalba4 <134150180+minivillalba4@users.noreply.github.com>
2025-10-03 23:01:19 +0200

    about me v2

--- src/data/author.ts
+++ src/data/author.ts
@@ -1,9 +1,3 @@
 import fotoLinkedin from '../assets/images/foto_linkedin.jpeg';
-export const author = {
-  name: 'Ismael Villalba',
-  avatar: { src: fotoLinkedin }, // aseguramos compatibilidad con el .src
-  url: '', // vacío para que no apunte a nada
-};
-
 export type Author = typeof author;
 
